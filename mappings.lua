4a62ed47579235d69108186cc54ac9564bcfb3aa
rezafikkri <fikkri.reza@gmail.com>
2023-12-20 15:11:08 +0700

    feat: custom mappings and disable relativenumber

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,10 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["\\"] = "",
+    ["|"] = "",
+    ["\\]"] = { "<Cmd>bnext<CR>", desc = "Next Buffer" },
+    ["\\["] = { "<Cmd>bprevious<CR>", desc = "Previous Buffer" },
   },
   t = {
     -- setting a mapping to false will disable it
